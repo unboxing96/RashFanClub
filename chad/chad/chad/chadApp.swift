a50aea60184cf67e01b54cb9985a70131a110c10
chad0909@naver.com <108044639+chad0909@users.noreply.github.com>
2023-07-04 00:47:58 +0900

    Feat: added Assets and font from Malty

---
+++ chad/chad/chad/chadApp.swift
@@ -0,0 +1,17 @@
+//
+//  chadApp.swift
+//  chad
+//
+//  Created by 077tech on 2023/07/04.
+//
+
+import SwiftUI
+
+@main
+struct chadApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
