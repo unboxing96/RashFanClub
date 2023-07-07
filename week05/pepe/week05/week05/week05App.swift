995303e7c0472c5ca94c728131c7189dce24b4d2
kimpepe <unboxing96@gmail.com>
2023-07-07 21:05:44 +0900

    Feat: 초기 설정 완료

---
+++ week05/pepe/week05/week05/week05App.swift
@@ -0,0 +1,17 @@
+//
+//  week05App.swift
+//  week05
+//
+//  Created by kimpepe on 2023/07/07.
+//
+
+import SwiftUI
+
+@main
+struct week05App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
