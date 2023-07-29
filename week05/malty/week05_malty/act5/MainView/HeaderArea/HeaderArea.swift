5e81ef79e164cfc87b89571eabbedf3720ef9a36
‘youihyonKim’ <insidesy4@gmail.com>
2023-07-29 09:49:02 +0900

    Feat : week05 구현 완료 - 말티

--- week05/malty/week05_malty/act5/MainView/HeaderArea/HeaderArea.swift
+++ week05/malty/week05_malty/act5/MainView/HeaderArea/HeaderArea.swift
@@ -24,7 +24,7 @@ struct HeaderArea: View {
 							.resizable()
 							.aspectRatio(contentMode: .fit)
 							.frame(width: UIScreen.main.bounds.width)
-							.opacity(getOpacity(for: product))
+							.opacity(getOpactiy(for: product, dragOffset: dragOffset, width: UIScreen.main.bounds.width))
 					}
 					
 					HStack(spacing: spacing) {
@@ -61,31 +61,17 @@ struct HeaderArea: View {
 			}
 		}
 	
-	private func offset(for banner: HeaderBanner, dragOffset: CGFloat, width: CGFloat) -> CGFloat {
-		//현재 보이는 배너기준 다른 배너들 - / + 처리 -> 총 8개의 index가 출력됨
-		let indexOffset = CGFloat(banner.index - currentIndex)
-		print("index Offset = \(indexOffset)")
+	private func getOpactiy(for banner: HeaderBanner, dragOffset: CGFloat, width: CGFloat) -> CGFloat {
+
+		let getEachBannerDisplayIndex = CGFloat(banner.index - currentIndex)
 		
-		//각 배너별 Offset 구함 -> 총 8개의 offset이 출력됨.
-		let totalOffset = indexOffset * (width - 1 * CGFloat(swipeProducts.count - 1))
-		print("totalOffset = \(totalOffset)")
+		let bannerOffset = getEachBannerDisplayIndex * width
 		
-		//드래그할때 실시간 offset 변화값을 가져오기위함 -> 실시간 변화된 값으로 opacity 값 설정
-		let dragOffsetClamped = max(-width, min(dragOffset, width))
-		print("dragOffsetClamped = \(totalOffset + dragOffsetClamped)")
-		return totalOffset + dragOffsetClamped
-	}
-	
-	private func getOpacity(for banner: HeaderBanner) -> Double {
-		//func offset에서 출력된 실시간 offset 값에서, device width 값을 나눠줌
-		//opacityValue -> 각 배너별 offset 값 / device width 값을 하면, 0 / 1 / 2 .. / 7의 값이 출력됨
-		let bannerOffset = offset(for: banner, dragOffset: dragOffset, width: UIScreen.main.bounds.width)
-		let opacityValue = (bannerOffset / UIScreen.main.bounds.width).magnitude
+		let bannerDragOffset = bannerOffset + dragOffset
 		
-		//출력된 opacityValue 에서 1 을 빼줘서, opcity의 값이 나오게됨.
-		//0 / 1 /  2 .. / 7 에서 1 - opacityValue 는
-		//1 / 0 / -1 .. /-6
-		return Double(1 - opacityValue)
+		let getOpactiy = (bannerDragOffset/UIScreen.main.bounds.width).magnitude
+
+		return Double(1 - getOpactiy)
 	}
 }
 
